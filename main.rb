643c4eedd9310c86e152a95004737ccdc3270293
EduardoKenji <eduardo.koketu@gmail.com>
2022-10-29 03:08:03 -0300

    Add rover creation

--- main.rb
+++ main.rb
@@ -1,3 +1,6 @@
+require './map'
+require './rover'
+
 # Return file data as an array of lines
 def get_file_data(file_name)
   file_path = File.join(__dir__, file_name)
@@ -12,10 +15,22 @@ def create_map(map_info)
   Map.new(0, 0, map_width, map_height)
 end
 
+def create_rover(rover_info)
+  rover_x = rover_info[0].to_i
+  rover_y = rover_info[1].to_i
+  rover_dir = rover_info[2]
+  Rover.new(rover_x, rover_y, rover_dir)
+end
+
 def main
   file_data = get_file_data('input.txt')
   map_info = file_data[0].split
   map = create_map(map_info)
+  (1..file_data.length - 2).step(2) do |i|
+    rover_info = file_data[i].split
+    rover = create_rover(rover_info)
+    rover_commands = file_data[i + 1].chars
+  end
 end
 
 main if __FILE__ == $PROGRAM_NAME
--- rover.rb
+++ rover.rb
@@ -1,9 +1,10 @@
 # NASA rover abstraction
 class Rover
-  def initialize(current_x, current_y)
+  def initialize(current_x, current_y, direction)
     @current_x = current_x
     @current_y = current_y
+    @direction = direction
   end
 
-  attr_accessor :current_x, :current_y
+  attr_accessor :current_x, :current_y, :direction
 end
