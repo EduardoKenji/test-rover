47014afda3a300d5aca4e8ba077a92d52b7a4c5a
EduardoKenji <eduardo.koketu@gmail.com>
2022-10-29 04:33:39 -0300

    Implement first working solution

--- map.rb
+++ map.rb
@@ -1,11 +1,25 @@
+require './rover'
+
 # Map abstraction to represent the plateau on Mars
 class Map
-  def initialize(starting_x, starting_y, width, height)
-    @starting_x = starting_x
-    @starting_y = starting_y
+  def initialize(width, height)
     @width = width
     @height = height
+    @grid = Array.new(height + 1) { Array.new(width + 1, nil) }
   end
 
-  attr_reader :starting_x, :starting_y, :width, :height
+  attr_reader :width, :height, :grid
+
+  def add_rover_to_map(rover)
+    @grid[rover.current_y][rover.current_x] = rover
+  end
+
+  def update_rover_position_inside_map(rover, new_x, new_y)
+    @grid[rover.current_y][rover.current_x] = nil
+    @grid[new_y][new_x] = rover
+  end
+
+  def grid_position_empty?(new_x, new_y)
+    @grid[new_y][new_x].nil?
+  end
 end
--- rover.rb
+++ rover.rb
@@ -7,4 +7,17 @@ class Rover
   end
 
   attr_accessor :current_x, :current_y, :direction
+
+  def rotate(command)
+    turn_to_direction_map = { 'N' => { 'L' => 'W', 'R' => 'E' },
+                              'W' => { 'L' => 'S', 'R' => 'N' },
+                              'S' => { 'L' => 'E', 'R' => 'W' },
+                              'E' => { 'L' => 'N', 'R' => 'S' } }
+    @direction = turn_to_direction_map[@direction][command]
+  end
+
+  def move_to_new_position(new_x, new_y)
+    @current_x = new_x
+    @current_y = new_y
+  end
 end
